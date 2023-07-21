d6011fd2792a16474c61b70d15e0740ed0b35bee
JJHernandez99 <kakihernandez1@gmail.com>
2023-07-21 14:13:09 -0300

    aplicacion actualizada

---
+++ microbloggin-app_final/microbloggin/urls.py
@@ -0,0 +1,33 @@
+"""microbloggin URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, re_path
+from twitter import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    re_path(r'^api/usuarios$', views.UserController.as_view()),
+    re_path(r'^api/login$', views.LoginController.as_view()),
+
+    
+    re_path(r'^api/usuarios/(?P<id>[0-9]+)$', views.usuario_detalle),
+    re_path(r'^api/publicaciones$', views.publicacion),
+    re_path(r'^api/perfil/(?P<id>[0-9]+)$', views.perfil),
+    re_path(r'^api/tendencia$', views.tendencia),
+    re_path(r'^api/publicaciones/(?P<id>[0-9]+)$', views.modificar_publicacion),
+
+]
---
+++ microbloggin-app_final/twitter/models.py
@@ -0,0 +1,73 @@
+from abc import abstractclassmethod
+from typing import ContextManager
+from django.db import models
+from django.db.models.base import ModelState
+from django.db.models.deletion import CASCADE
+from django.db.models.fields.mixins import FieldCacheMixin
+#from jsonfield import JSONField
+
+# Create your models here.
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre_usuario = models.CharField(max_length=255, default='')
+    contraseña=models.CharField(max_length=255)
+    telefono = models.PositiveIntegerField(default=99999999)
+    nombre = models.CharField(max_length=255, default='')
+    apellido = models.CharField(max_length=255, default='')
+    TIPOS_SEXO = [
+        ('MASCULINO', 'Masculino'),
+        ('FEMENINO', 'Femenino'),
+        ('OTRO', 'Otro')]
+    sexo = models.CharField(
+        max_length=14,
+        choices=TIPOS_SEXO,
+        default='Masculino',
+    )
+    fecha_nacimiento = models.DateField(default="1999-01-01")
+    email = models.EmailField(max_length=254, default="ejemplo@gmail.com")
+
+    class Meta:
+        abstract = True
+
+class Usuario(User):
+    seguidores = models.JSONField(default=None,blank=True,null=True)
+    seguidos = models.JSONField(default=None,blank=True,null=True)
+
+    def __str__(self):
+        return f'NombreUsuario: {self.nombre_usuario}, Email: {self.email}'
+    
+    def login(self,contraseña):
+        return contraseña==self.contraseña
+
+class Administrador(User):
+    status = models.BooleanField(default=True)
+
+class Publicacion(models.Model):
+    id = models.AutoField(primary_key=True)
+    usuario = models.ForeignKey(Usuario, on_delete=CASCADE, related_name='realizada')
+    contenido = models.CharField(max_length=254, default='')
+    etiqueta = models.JSONField(default="", null=True, blank=True)
+    mencion = models.ForeignKey(Usuario, null=True, blank=True, on_delete=CASCADE, default=None, related_name='menciona')
+    fecha = models.DateField(auto_now_add=True)
+    republicacion = models.ForeignKey('self', null=True, blank=True, on_delete=CASCADE, related_name='republica')
+    is_edited = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return f'Publicacion: {self.contenido}, Usuario: {self.usuario.nombre_usuario}'
+    
+class Tendencias(models.Model):
+    id = models.AutoField(primary_key=True) 
+    #fecha
+    etiqueta = models.ForeignKey(Publicacion, on_delete=CASCADE)
+    #contador
+
+class MensajePriv(models.Model):
+    id = models.AutoField(primary_key=True)
+    fecha = models.DateField(auto_now_add=True)
+    contenido = models.CharField(max_length=254, default='')
+    receptor = models.ForeignKey(Usuario, on_delete=CASCADE, related_name= 'recibe')
+    emisor = models.ForeignKey(Usuario, on_delete=CASCADE, related_name= 'emite')
+
+
